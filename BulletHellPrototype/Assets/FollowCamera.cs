080d0ea1bfb4fa7895ebe55c6bc83fbce98b9d74
samuelhegner <C16439262@mydit.ie>
2019-05-04 12:54:26 +0100

    Camera tracking and project

---
+++ BulletHellPrototype/Assets/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    Vector3 offsetPosition;
+
+    public float camSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetPosition = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offsetPosition, Time.deltaTime * camSpeed);
+        transform.LookAt(player.transform);
+    }
+}
